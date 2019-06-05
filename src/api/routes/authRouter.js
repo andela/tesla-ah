ae8e9ed2675f987d2dc45a32d2533d870c7cf94d
UhiriweAudace <u.audace@gmail.com>
2019-06-05 16:51:47 +0200

    [chore #166240982] re-setting up of the Eslint and re-touch on some code line

--- src/api/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import authController from '../controllers/auth';
-
-const authRouter = Router();
-const { signup } = authController;
-
-authRouter.post('/signup', signup);
-
-export default authRouter;
--- src/api/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import authRouter from './authRouter';
-
-const api = express();
-
-// Routers go here
-api.use('/auth', authRouter);
-
-export default api;
