0cc3f9a5b02e2bee3b3b54b84b2a108216b8ddf3
Diama1 <32199791+Diama1@users.noreply.github.com>
2019-06-06 11:37:06 +0200

    to create a coverage script and install all related dependecies

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: NODE_ENV=staging npm serve
---
+++ dist/api/controllers/auth.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  signup: function signup() {}
+};
+exports["default"] = _default;
