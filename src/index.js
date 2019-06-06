5080bb2f0efaf7c4fb9cd27ba4e95f1f237e68a2
Emmanuel Rukundo <emmanuelrukundo@Emmanuels-MacBook-Pro-2.local>
2019-06-06 10:22:56 +0200

    chore(swagger):add swagger doc

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
 import api from './api/routes';
 import globalMiddleware from './middleware/globalMiddleware';
+import  swaggerDoc from '../swagger.json';
+
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
+
+app.use('/', swaggerUi.serve,swaggerUi.setup(swaggerDoc));
+
 
 globalMiddleware(app);
 
