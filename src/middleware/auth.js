c02867299a40f5d6f6a1138603d2628468ef632b
rukundoeric <ericrukundo005@gmail.com>
2019-06-07 13:54:56 +0200

    [Bug houndb-bug] jsdoc missinng corrected

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,21 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable class-methods-use-this */
 import authHelper from '../api/helpers/Token.helper';
 import db from '../../models';
 
 const { users } = db;
+/**
+ * @class Auth
+ * @description Authentication based class
+ *  */
 class Auth {
+  /**
+   * Verify token middleware
+   * @param {Object} req - Request
+   * @param {Object} res  - Response
+   * @param {Function} next -Next
+   * @returns {Object} The response object
+   */
   async verifyToken(req, res, next) {
     const bearerToken = req.headers.authorization;
     const token = bearerToken.split(' ')[1];
@@ -13,27 +25,30 @@ class Auth {
         error: 'Token is not provided'
       });
     }
-    authHelper.decodeToken(token).then((user) => {
-      users
-        .findAll({
-          where: {
-            id: user.userId
-          }
-        })
-        .then((result) => {
-          if (!result[0]) {
-            return res.status(404).send({
-              status: 404,
-              error: { message: 'The token you provided is invalid' }
-            });
-          }
-          req.user = { userId: result[0].id };
-          next();
-        });
-    }).catch(error => res.status(400).send({
-      status: 400,
-      error
-    }));
+    authHelper
+      .decodeToken(token)
+      .then((user) => {
+        users
+          .findAll({
+            where: {
+              id: user.userId
+            }
+          })
+          .then((result) => {
+            if (!result[0]) {
+              return res.status(404).send({
+                status: 404,
+                error: { message: 'The token you provided is invalid' }
+              });
+            }
+            req.user = { userId: result[0].id };
+            next();
+          });
+      })
+      .catch(error => res.status(400).send({
+        status: 400,
+        error
+      }));
   }
 }
 export default new Auth();
