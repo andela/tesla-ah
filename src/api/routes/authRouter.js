3337f921f14fc9dfff1fc00c71ee944e9a46188e
Mireille Niwemuhuza <mireilleniwemuhuza@Mireilles-MacBook-Pro.local>
2019-06-10 14:27:08 +0200

    feat: add registration validation errors

--- src/api/routes/authRouter.js
+++ src/api/routes/authRouter.js
@@ -3,10 +3,10 @@ import authController from '../controllers/auth';
 import validateBody from '../../middleware/userValidations';
 import userValidation from '../../middleware/validUser';
 
-
 const authRouter = Router();
 const { signup } = authController;
+const { usernameExists, emailExists } = userValidation;
 
-authRouter.post('/signup', validateBody('signup'), userValidation.usernameExists, userValidation.emailExists, signup);
+authRouter.post('/signup', validateBody('signup'), usernameExists, emailExists, signup);
 
 export default authRouter;
---
+++ src/sequelize/migrations/20190611080237-create-user.js
@@ -0,0 +1,48 @@
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    firstName: {
+      type: Sequelize.STRING
+    },
+    lastName: {
+      type: Sequelize.STRING
+    },
+    username: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    bio: {
+      type: Sequelize.STRING
+    },
+    image: {
+      type: Sequelize.STRING
+    },
+    dateOfBirth: {
+      type: Sequelize.DATE
+    },
+    gender: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('Users')
+};
