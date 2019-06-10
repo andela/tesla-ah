2b4943c796bc73930f0cc7605506706fd2ba110f
Eric Prestein <45217364+rukundoeric@users.noreply.github.com>
2019-06-10 20:03:03 +0200

    #166240989 Verfy token (#14)
    * [Feature #166240989] Enable verifyb token
    
    * hide nyc_output
    
    * [Feature
    
    * [Feature
    
    * [Bug houndb-bug] jsdoc missinng corrected
    
    * [Merge conflict] resolve merge conflict
    
    * [Bug hound] corrected hound violation
    
    * [Bug readme] ordering budges
    
    * ft-166220989 - Add swagger
    
    * chore(postgres): configure sequelize-cli with es6
    
    * chore(postgres): fix HoundCI error
    
    * [Hound violations] reslove hound violations
    
    * [Hound violations] reslove hound violations
    
    * [Feature #166240989]solve database connection problems
    
    * [Feature #166240989]solve database connection problems

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,25 @@
-const config = {};
+import dotenv from 'dotenv';
 
+const config = {};
+dotenv.config();
 config.development = {
-  dbUrl: process.env.DEV_DATABASE_URL,
-  dialect: 'postgres',
+  username: process.env.LOCAL_DB_USER,
+  password: process.env.LOCAL_DB_PASSWORD,
+  database: process.env.LOCAL_DB_NAME,
+  host: '127.0.0.1',
+  dialect: 'postgres'
 };
 
 config.staging = {
-  dbUrl: process.env.DATABASE_URL,
+  use_env_variable: 'DATABASE_URL',
 };
 
 config.test = {
-  dbUrl: process.env.TEST_DATABASE_URL,
+  username: process.env.TEST_DB_USER,
+  password: process.env.TEST_DB_PASSWORD,
+  database: process.env.TEST_DB_NAME,
+  host: '127.0.0.1',
+  dialect: 'postgres'
 };
 
 config.production = {
