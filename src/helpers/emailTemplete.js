9b39eea66bd2d4a7b4165c509a7ec7b64bf37fa6
Eric Prestein <45217364+rukundoeric@users.noreply.github.com>
2019-06-15 22:06:16 +0200

    feat(password-reset): Enable user to reset password (#25)

---
+++ src/helpers/emailTemplete.js
@@ -0,0 +1,48 @@
+/**
+ * @class ResetPassword
+ * @description Authentication based class
+ *  */
+class Templete {
+/**
+  * Verify token middleware
+  * @param {String} firstname - Request
+  * @param {String} lastname  - Response
+  * @param {String} token -EmailTemplete
+  * @returns {String} The response String
+  */
+  static getPasswordResetTemplete(firstname, lastname, token) {
+    return `
+    <div style="background:#e5eeff;width:100%;padding:20px 0;">
+    <div style="max-width:760px;margin:0 auto;background:#ffffff">
+    <div style="background:#266cef;padding:10px;color:#ffffff;text-align:center;font-size:34px">
+    Authors Haven
+    </div>
+    <div style="padding:20px;text-align:left;">
+    <p>
+    <h2>Hi ${firstname} ${lastname} </h2></br>
+    You Recently requested a password reset for your Authors Haven Account, Click the the Button below
+    to reset it.</br>
+    <form  action="${token}">
+      <button style="background:#266cef;padding:10px; outline: none; border:0; border-radius:10px; margin:5px;color:#ffffff;text-align:center;font-size:13px">Reset your Password</button>
+    </form>
+    </br>
+    If you did not request a password reset please ignore this email or reply to let us know.
+    This link will be expired in next 10 minutes.
+    </p>
+    <a href="https://andela.com">Visit Andela's website</a>
+    </div>
+    <br>
+    <div style="padding:20px;text-align:left;">
+    <b>Authors Haven</b>
+    </div>
+    </div>
+    <div style="padding:35px 10px;text-align:center;">
+    Copyright, 2019<br>
+    Authors Haven 
+    </div>
+    </div>
+      `;
+  }
+}
+
+export default Templete;
