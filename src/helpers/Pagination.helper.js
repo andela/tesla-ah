1f5f8d3a07c71bcebec7e9deb13d44920fb5d31a
rukundoeric <ericrukundo005@gmail.com>
2019-06-17 18:41:54 +0200

    mend

---
+++ src/helpers/Pagination.helper.js
@@ -0,0 +1,17 @@
+/**
+ * @author Eric rukundo
+ * @class Pagination
+ * @description this class performs the whole authentication
+ */
+class Pagination {
+  /**
+   * signup controller
+   * @param {Integer} page  - Response
+   * @returns {Object} The response object
+   */
+  static async pagianate(page) {
+    return page;
+  }
+}
+
+export default Pagination;
