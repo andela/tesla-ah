199a486540473b90e8c179957e74ae75e6407628
Elie Mugenzi <eliemugenzi@gmail.com>
2019-06-20 13:20:41 +0200

    feat(pagination): Implementating pagination for articles (#41)

--- src/api/controllers/articlesController.js
+++ src/api/controllers/articlesController.js
@@ -42,6 +42,24 @@ class articlesController {
    * @return {object} returns array of articles
    */
   static async getAllArticle(req, res) {
+    const { page, limit } = req.query;
+    const pageNumber = parseInt(page, 10);
+    const limitNumber = parseInt(limit, 10);
+    if (typeof pageNumber === 'number' && typeof limitNumber === 'number' && typeof page !== 'undefined' && typeof limit !== 'undefined') {
+      if (pageNumber <= 0 || limitNumber <= 0) {
+        return res.status(400).json({
+          error: 'Invalid request'
+        });
+      }
+      const offset = limitNumber * (pageNumber - 1);
+      const foundArticles = await Article.findAll({
+        limit: limitNumber,
+        offset
+      });
+      return res.json({
+        data: foundArticles
+      });
+    }
     const allArticle = await articles.getAllArticle();
 
     if (!allArticle[0]) return res.status(404).send({ error: 'Whoops! No Articles found!' });
