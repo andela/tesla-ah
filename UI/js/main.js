542fa05aa2617be4660f81dc31aea101e68e3441
rukundoeric <ericrukundo005@gmail.com>
2019-07-07 20:48:17 +0200

    some changes

--- UI/js/main.js
+++ UI/js/main.js
@@ -2,7 +2,9 @@
 $(document).scroll(() => {
   if ($(document).scrollTop() > 250) {
     $('header').addClass('shrink');
+    $('menu_button').addClass('shrink-menu-button');
   } else {
     $('header').removeClass('shrink');
+    $('menu_button').removeClass('shrink-menu-button');
   }
 });
