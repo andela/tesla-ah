0f620edb40f43d82a3a61161e914df91bdfd7dd4
Deschant Kounou <50397889+deschantkn@users.noreply.github.com>
2019-07-08 18:22:17 +0200

    fix(heroku): add regenerator runtime import to workers (#70)
    - reduce email sending cronjob from 10 to 5 minutes

--- src/workers/uploadImage.js
+++ src/workers/uploadImage.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+import 'regenerator-runtime';
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 import models from '../sequelize/models';
