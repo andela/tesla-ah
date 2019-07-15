ab951848ab56ed68dfd0e4ea56ca8ddf8dace45a
Deschant Kounou <50397889+deschantkn@users.noreply.github.com>
2019-07-15 19:47:37 +0200

    feat(image-upload): enable image upload on articles (#72)
    - add image upload support when creating and updating articles
    - add multiple image upload support
    
    Finishes [#167194031]

--- src/workers/uploadImage.js
+++ src/workers/uploadImage.js
@@ -2,9 +2,11 @@
 import 'regenerator-runtime';
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
+import { forEach } from 'lodash';
+import { each } from 'async';
 import models from '../sequelize/models';
 
-const { User } = models;
+const { User, Article } = models;
 
 
 dotenv.config();
@@ -16,12 +18,43 @@ cloudinary.config({
 });
 
 
-const uploadHelper = async (filePath, user) => {
+const uploadHelper = async (uploads, id, model) => {
   console.log(`PID: ${process.pid} === UPLOADING IMAGE ===`);
-  const uploadedImage = await cloudinary.uploader.upload(filePath.path);
+  let uploadedImage;
 
-  await User.update({ image: uploadedImage.secure_url }, { where: { id: user } });
-  console.log('=== FINISHED UPLOADING IMAGE ===');
+  forEach(uploads, async (upload) => {
+    const gallery = [];
+
+    switch (model) {
+      case 'user':
+        forEach(upload, async (file) => {
+          const { path, fieldname } = file;
+          uploadedImage = await cloudinary.uploader.upload(path);
+          await User.update({ [fieldname]: uploadedImage.secure_url }, { where: { id } });
+          console.log('UPLOAD COMPLETED');
+        });
+        break;
+
+      case 'article':
+        each(upload, async (file, callback) => {
+          const { path } = file;
+          uploadedImage = await cloudinary.uploader.upload(path);
+          gallery.push(uploadedImage.secure_url);
+          callback();
+        }, async (err) => {
+          if (!err) {
+            const article = await Article.findOne({ where: { id } });
+            const newGallery = [...article.gallery, ...gallery];
+            await Article.update({ gallery: newGallery }, { where: { id } });
+            console.log('UPLOAD COMPLETED');
+          }
+        });
+        break;
+
+      default:
+        break;
+    }
+  });
 };
 
 module.exports = uploadHelper;
