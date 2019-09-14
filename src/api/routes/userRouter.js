7f7d7e994b542f9402270e049da95c1cab8e2efc
Deschant Kounou <deschant.kounou@andela.com>
2019-09-14 16:15:24 +0200

    Fix username uniqueness issue

--- src/api/routes/userRouter.js
+++ src/api/routes/userRouter.js
@@ -4,6 +4,7 @@ import Auth from '../../middleware/auth';
 import validateBody from '../../middleware/validateBody';
 import upload from '../../handlers/multer';
 import OptController from '../controllers/optController';
+import validUser from '../../middleware/validUser';
 
 const userRouter = Router();
 const { updateProfile, deleteProfile, getCurrentUser } = ProfilesController;
@@ -11,6 +12,7 @@ const {
   optOutEmail, optOutApp, OptInApp, OptInEmail, OptedInApp, OptedInEmail
 } = OptController;
 const { verifyToken, checkOwnership, checkIsAdmin } = Auth;
+const { usernameExists } = validUser;
 
 userRouter.get('/', verifyToken, getCurrentUser);
 userRouter.post('/optinemail', verifyToken, OptInEmail);
@@ -19,7 +21,7 @@ userRouter.delete('/optinemail', verifyToken, optOutEmail);
 userRouter.delete('/optinapp', verifyToken, optOutApp);
 userRouter
   .route('/:id')
-  .put(verifyToken, checkOwnership, upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), validateBody('updateUser'), updateProfile)
+  .put(verifyToken, checkOwnership, upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), validateBody('updateUser'), usernameExists, updateProfile)
   .delete(verifyToken, checkIsAdmin, deleteProfile);
 
 userRouter.get('/optinapp', verifyToken, OptedInApp);
