e57b52e259369bff9bc273f1be82be64cdec26f5
Deschant Kounou <deschant.kounou@andela.com>
2019-09-16 14:15:36 +0200

    remove username middleware

--- src/api/routes/userRouter.js
+++ src/api/routes/userRouter.js
@@ -4,7 +4,6 @@ import Auth from '../../middleware/auth';
 import validateBody from '../../middleware/validateBody';
 import upload from '../../handlers/multer';
 import OptController from '../controllers/optController';
-import validUser from '../../middleware/validUser';
 
 const userRouter = Router();
 const { updateProfile, deleteProfile, getCurrentUser } = ProfilesController;
@@ -12,7 +11,6 @@ const {
   optOutEmail, optOutApp, OptInApp, OptInEmail, OptedInApp, OptedInEmail
 } = OptController;
 const { verifyToken, checkOwnership, checkIsAdmin } = Auth;
-const { usernameExists } = validUser;
 
 userRouter.get('/', verifyToken, getCurrentUser);
 userRouter.post('/optinemail', verifyToken, OptInEmail);
@@ -21,7 +19,7 @@ userRouter.delete('/optinemail', verifyToken, optOutEmail);
 userRouter.delete('/optinapp', verifyToken, optOutApp);
 userRouter
   .route('/:id')
-  .put(verifyToken, checkOwnership, upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), validateBody('updateUser'), usernameExists, updateProfile)
+  .put(verifyToken, checkOwnership, upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), validateBody('updateUser'), updateProfile)
   .delete(verifyToken, checkIsAdmin, deleteProfile);
 
 userRouter.get('/optinapp', verifyToken, OptedInApp);
